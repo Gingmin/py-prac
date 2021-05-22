c67480138cd4a0e10c8fd6a3c73465e5dbd58f41
Gingmin <kkmk220@gmail.com>
2021-05-22 14:07:36 +0900

    long time

---
+++ byme.py
@@ -0,0 +1,10 @@
+# class Sign:
+#     def sign(self):
+#         print("이 프로그램은 김민기에 의해 만들어졌습니다.")
+#         print("유튜브 : http://youtube.com")
+#         print("이메일 : kkmk220@naver.com")
+
+def sign():
+    print("이 프로그램은 김민기에 의해 만들어졌습니다.")
+    print("유튜브 : http://youtube.com")
+    print("이메일 : kkmk220@naver.com")   
---
+++ restart-Python.py
@@ -0,0 +1,16 @@
+# print("신씨가 소리질렀다. \"도둑이야\".")
+# print("naver", "kakao", "samsung", sep="/")
+# print("first", end="/");print("second")
+# print(5/3)
+
+# 시가총액 = 298000000000000
+# 현재가 = 5000
+# PER = 15.79
+# print(시가총액, type(시가총액))
+# print(현재가, type(현재가))
+# print(PER, type(PER))
+
+# s = "hello"
+# t = "python"
+# print(s, t)
+# print(s+"!", t)
